e2640ae78bf547b8c2f74f3da51a124f6221f4c6
chenjumin <chenjumin@163.com>
2019-12-13 22:10:26 +0800

    1

--- seasy-cloud-frontweb/src/main/java/com/seasy/cloud/frontweb/controller/FrontwebController.java
+++ seasy-cloud-frontweb/src/main/java/com/seasy/cloud/frontweb/controller/FrontwebController.java
@@ -1,10 +1,17 @@
 package com.seasy.cloud.frontweb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seasy.cloud.base.Address;
+import com.seasy.cloud.base.User;
 import com.seasy.cloud.frontweb.feignclient.UserFeignClient;
 
 @RestController
@@ -14,9 +21,21 @@ public class FrontwebController {
 
 	@GetMapping("/user/{id}")
 	public String getUser(@PathVariable(value="id") Long id){
-		String result = userFeignClient.get(id);
-		System.out.println("result=" + result);
-		result = result + " : " + System.currentTimeMillis();
-		return result;
+		return userFeignClient.get(id) + " : " + System.currentTimeMillis();
+	}
+	
+	@GetMapping("/user/query")
+	public String query(@RequestParam(value="name") String name){
+		return userFeignClient.query(name);
+	}
+
+	@PostMapping("/user/add")
+	public User add(@RequestBody User user){
+		return userFeignClient.add(user);
+	}
+	
+	@GetMapping("/address/list")
+	public List<Address> getAllAddress(){
+		return userFeignClient.getAllAddress();
 	}
 }
