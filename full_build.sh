e5455c0e6db8e1699873cc8793e37ec86cea4b25
chenjumin <chenjumin@163.com>
2019-12-29 23:35:40 +0800

    1

--- full_build.sh
+++ full_build.sh
@@ -11,7 +11,7 @@ cd /opt/projects/seasy-cloud/seasy-cloud-eureka
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/seasy-cloud-eureka:0.0.1-SNAPSHOT
+docker push 192.168.134.147/seasy/seasy-cloud-eureka:0.0.1-SNAPSHOT
 !
 
 :<<!
@@ -23,26 +23,26 @@ cd /opt/projects/seasy-cloud/seasy-cloud-adminserver
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/seasy-cloud-adminserver:0.0.1-SNAPSHOT
+docker push 192.168.134.147/seasy/seasy-cloud-adminserver:0.0.1-SNAPSHOT
 
 cd /opt/projects/seasy-cloud/seasy-cloud-userservice
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/seasy-cloud-userservice:0.0.1-SNAPSHOT
+docker push 192.168.134.147/seasy/seasy-cloud-userservice:0.0.1-SNAPSHOT
 
 
 cd /opt/projects/seasy-cloud/seasy-cloud-frontweb
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/seasy-cloud-frontweb:0.0.1-SNAPSHOT
+docker push 192.168.134.147/seasy/seasy-cloud-frontweb:0.0.1-SNAPSHOT
 
 cd /opt/projects/seasy-cloud/seasy-cloud-gateway
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/seasy-cloud-gateway:0.0.1-SNAPSHOT
+docker push 192.168.134.147/seasy/seasy-cloud-gateway:0.0.1-SNAPSHOT
 !
 
 :<<!
@@ -50,18 +50,18 @@ cd /opt/projects/seasy-cloud/seasy-cloud-config-apollo/apollo-configservice
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/apollo-configservice:1.5.1
+docker push 192.168.134.147/seasy/apollo-configservice:1.5.1
 
 cd /opt/projects/seasy-cloud/seasy-cloud-config-apollo/apollo-adminservice
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/apollo-adminservice:1.5.1
+docker push 192.168.134.147/seasy/apollo-adminservice:1.5.1
 
 cd /opt/projects/seasy-cloud/seasy-cloud-config-apollo/apollo-portal
 chmod 777 build.sh
 ./build.sh
 
-docker push 192.168.134.142/seasy/apollo-portal:1.5.1
+docker push 192.168.134.147/seasy/apollo-portal:1.5.1
 !
 
