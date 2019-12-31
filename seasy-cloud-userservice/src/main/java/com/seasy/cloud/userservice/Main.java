679ef36d2b2e59c4340883eb1ea54c390ee5615b
chenjumin <chenjumin@163.com>
2019-12-31 16:03:51 +0800

    1

--- seasy-cloud-userservice/src/main/java/com/seasy/cloud/userservice/Main.java
+++ seasy-cloud-userservice/src/main/java/com/seasy/cloud/userservice/Main.java
@@ -3,11 +3,19 @@ package com.seasy.cloud.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import com.seasy.cloud.base.web.DefaultWebMvcConfig;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+	
+	@Bean
+	public DefaultWebMvcConfig getDefaultWebMvcConfig(){
+		return new DefaultWebMvcConfig();
 	}
 }
